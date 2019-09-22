e6ac43000fb0d82d8333db1d0330170dbd998853
Ryan Carniato <ryansolid@gmail.com>
2019-09-21 22:19:20 -0700

    Add simple SVG test

---
+++ test/svg.spec.jsx
@@ -0,0 +1,34 @@
+import * as S from 's-js';
+import { exportAllDeclaration } from '@babel/types';
+
+describe('create simple svg', () => {
+  it('Ensure dynamic props are set as attributes', () => {
+    let rect;
+    const x = S.data(0),
+      y = S.data(0),
+      style = S.data({
+        fill: 'red',
+        stroke: 'black',
+        'stroke-width': 5,
+        opacity: 0.5
+      });
+    function Component() {
+      return <svg width="400" height="180">
+        <rect
+          x={( x() )}
+          y={( y() )}
+          ref={rect}
+          width="150"
+          height="150"
+          style={( style() )}
+        />
+      </svg>
+    }
+
+    S.root(() => <Component />);
+    expect(rect.outerHTML).toBe(`<rect width="150" height="150" x="0" y="0" style="fill: red; stroke: black; stroke-width: 5; opacity: 0.5;"></rect>`);
+    x(10);
+    y(50);
+    expect(rect.outerHTML).toBe(`<rect width="150" height="150" x="10" y="50" style="fill: red; stroke: black; stroke-width: 5; opacity: 0.5;"></rect>`);
+  });
+});
