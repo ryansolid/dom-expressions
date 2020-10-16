1076dade9c2b36e1acd26582d486bbf39cba674b
Ryan Carniato <ryansolid@gmail.com>
2020-10-16 02:59:55 -0700

    fix types

--- packages/dom-expressions/src/runtime.d.ts
+++ packages/dom-expressions/src/runtime.d.ts
@@ -48,8 +48,9 @@ declare type AttributeInfo = {
     alias?: string;
   };
 };
-export const Attributes: AttributeInfo;
-export const SVGAttributes: AttributeInfo;
+export const Aliases: Record<string, string>;
+export const Properties: Set<string>;
+export const ChildProperties: Set<string>;
 export const NonComposedEvents: Set<string>;
 export const SVGElements: Set<string>;
 export const SVGNamespace: Record<string, string>;
