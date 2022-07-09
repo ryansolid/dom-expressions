94905e6c3efb149ddbc8ffb7d401e333af0d210a
Joe Pea <joe@trusktr.io>
2022-07-09 06:29:11 +0300

    Handle special case attributes (#146)
    * add tests for special case attributes including multi-line attributes, same-line boolean attributes, attributes with symbols including quotes in their name, and attributes without quotes but with any symbols including quotes or equals signs in their values. eliminate double escaping in regexes. some code was formatted by prettier
    
    * remove regexr, revert to previous regex strings
    
    * ignore package-lock.json
    
    * update html-parse-string version
    
    * add prepare scripts to make it possible to install packages from git (f.e. using https://gitpkg.vercel.app for installing from sub-folders)
    
    * add one more test case for attributes with double quote chars

--- packages/lit-dom-expressions/test/html.spec.js
+++ packages/lit-dom-expressions/test/html.spec.js
@@ -44,9 +44,13 @@ describe("Test HTML", () => {
           ref=${el => {
             el.setAttribute("refset", "true");
           }}
-          ...${() => ({title: "hi"})}
+          ...${() => ({ title: "hi" })}
         >
-          <h1 class=${welcoming} title="${welcoming} John ${"Smith"}" style=${() => ({ "background-color": "red" })}>
+          <h1
+            class=${welcoming}
+            title="${welcoming} John ${"Smith"}"
+            style=${() => ({ "background-color": "red" })}
+          >
             <a href="/" ref=${r => (link = r)}>Welcome</a>
           </h1>
         </div>
@@ -109,7 +113,7 @@ describe("Test HTML", () => {
       html` <div>${() => props.name + " " + props.middle}${props.children}</div> `;
     S.root(() => {
       const template = html`
-        <div id="main" ...${() => ({title: "hi"})}>
+        <div id="main" ...${() => ({ title: "hi" })}>
           <${Comp} name=${() => "John"} middle="R."><span>Smith</span><//>
           <div>After</div>
         </div>
@@ -146,18 +150,18 @@ describe("Test HTML", () => {
     const Match = props => props;
     S.root(() => {
       const template = html`<div>
-      <${Switch}>
-        <${Match} when=${1}>
-          <div>Hi</div>
+        <${Switch}>
+          <${Match} when=${1}>
+            <div>Hi</div>
+          <//>
+          <${Match} when=${2}>
+            <div>Morris</div>
+          <//>
+          <${Match} when=${3}>
+            <p>Jamie</p>
+          <//>
         <//>
-        <${Match} when=${2}>
-          <div>Morris</div>
-        <//>
-        <${Match} when=${3}>
-          <p>Jamie</p>
-        <//>
-      <//>
-    </div>`;
+      </div>`;
       const div = document.createElement("div");
       div.appendChild(template);
       expect(div.innerHTML.replace(/<!--#-->/g, "")).toBe(FIXTURES[6]);
@@ -175,26 +179,95 @@ describe("Test HTML", () => {
     div.appendChild(template);
     expect(div.innerHTML.replace(/<!--#-->/g, "")).toBe(FIXTURES[7]);
   });
-});
 
-test("Test style tag", () => {
-  const color = "red";
-  // prettier-ignore
-  const template = html`
-    <style>.something{color:${color}}</style>
-  `;
-  const div = document.createElement("div");
-  div.appendChild(template);
-  expect(div.innerHTML.replace(/<!--#-->/g, "")).toBe(FIXTURES[8]);
-});
+  test("multi-line, strange-but-valid, and edge-case attributes", () => {
+    // This tests some attributes that previously did not work (but not all of
+    // them, because html template tag still stumbles on the uncommon ones
+    // (TODO)), detailed in
+    // https://github.com/ryansolid/dom-expressions/issues/143 and
+    // https://github.com/ryansolid/html-parse-string/issues/3
 
-test("Test double toggle classList", () => {
-  S.root(() => {
-    const d = S.data("first");
-    const template = html` <div classList=${() => ({ [d()]: true })} /> `;
+    /** @type {HTMLDivElement} */
+    // prettier-ignore
+    // We need to not format this HTML code otherwise prettier changes or even breaks the special cases we're testing.
+    const div = html`
+      <div
+        multiline="
+          foo
+          bar
+        "
+        multi-line="
+          foo
+          bar
+        "
+        lorem ipsum these-all-need-to-be-on-the-same-line
+        baz=123=456
+        #$%=123
+      ></div>
+    `;
+
+    // TODO these attributes are parsed correctly by html-parse-string now, but
+    // html template tag doesn't handle them well yet.
+    // beep"boop
+    // y-o=123"456=#$%
+    // #$%'123-
+
+    // It was previously creating attributes from each new line.
+    expect(div.attributes.getNamedItem("foo")).toBe(null);
+    expect(div.attributes.getNamedItem("bar")).toBe(null);
+
+    // Such attributes should now be preserved.
+    expect(div.attributes.getNamedItem("multiline")?.value).toBe(
+      `
+          foo
+          bar
+        `
+    );
+
+    expect(div.attributes.getNamedItem("multi-line")?.value).toBe(
+      `
+          foo
+          bar
+        `
+    );
+
+    expect(div.attributes.getNamedItem("lorem")?.value).toBe("");
+    expect(div.attributes.getNamedItem("ipsum")?.value).toBe("");
+    expect(div.attributes.getNamedItem("these-all-need-to-be-on-the-same-line")?.value).toBe("");
+    expect(div.attributes.getNamedItem("baz")?.value).toBe("123=456");
+    expect(div.attributes.getNamedItem("#$%")?.value).toBe("123");
+
+    // TODO
+    // expect(div.attributes.getNamedItem('beep"boop')?.value).toBe("");
+    // expect(div.attributes.getNamedItem("y-o")?.value).toBe('123"456=#$%');
+    // expect(div.attributes.getNamedItem("#$%'123-")?.value).toBe("");
+
+    // ensure it handles `"` chars correctly
+    const el = html`<lume-box uniforms='{ "iTime": { "value": 0 } }'></lume-box>`;
+
+    expect(el.attributes.length).toBe(1);
+    expect(el.attributes.uniforms?.value).toBe('{ "iTime": { "value": 0 } }');
+  });
+
+  test("Test style tag", () => {
+    const color = "red";
+    // prettier-ignore
+    const template = html`
+      <style>.something{color:${color}}</style>
+    `;
     const div = document.createElement("div");
     div.appendChild(template);
-    d("second");
-    expect(div.innerHTML.replace(/<!--#-->/g, "")).toBe(FIXTURES[9]);
+    expect(div.innerHTML.replace(/<!--#-->/g, "")).toBe(FIXTURES[8]);
+  });
+
+  test("Test double toggle classList", () => {
+    S.root(() => {
+      const d = S.data("first");
+      const template = html` <div classList=${() => ({ [d()]: true })} /> `;
+      const div = document.createElement("div");
+      div.appendChild(template);
+      d("second");
+      expect(div.innerHTML.replace(/<!--#-->/g, "")).toBe(FIXTURES[9]);
+    });
   });
 });
