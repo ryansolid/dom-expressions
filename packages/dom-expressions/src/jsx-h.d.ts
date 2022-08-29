e58e05b40eab5fea8c3d1099d52bf47927316b6c
Ryan Carniato <ryansolid@gmail.com>
2022-08-28 22:55:07 -0700

    stricter override

--- packages/dom-expressions/src/jsx-h.d.ts
+++ packages/dom-expressions/src/jsx-h.d.ts
@@ -286,7 +286,7 @@ export namespace JSX {
 
   interface CSSProperties extends csstype.PropertiesHyphen {
     // Override
-    [key: string]: string | number | undefined;
+    [key: `—${string}`]: string | number | undefined
   }
 
   type HTMLAutocapitalize = "off" | "none" | "on" | "sentences" | "words" | "characters";
