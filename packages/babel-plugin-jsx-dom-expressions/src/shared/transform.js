85b14644d057d04169157cbeec3edbcad2c03bfc
ULIVZ <chenhaoli@bytedance.com>
2023-03-03 07:54:44 +0800

    refactor: simplify logic of find tag renderer (#207)

--- packages/babel-plugin-jsx-dom-expressions/src/shared/transform.js
+++ packages/babel-plugin-jsx-dom-expressions/src/shared/transform.js
@@ -163,13 +163,9 @@ export function transformElement(config, path, info = {}) {
   // <div ...></div>
   // const element = getTransformElemet(config, path, tagName);
 
-  let tagRenderer;
-  for (var renderer of config.renderers ?? []) {
-    if (renderer.elements.indexOf(tagName) !== -1) {
-      tagRenderer = renderer;
-      break;
-    }
-  }
+  const tagRenderer = (config.renderers ?? []).find(
+    renderer => renderer.elements.includes(tagName)
+  )
 
   if (tagRenderer?.name === "dom" || getConfig(path).generate === "dom") {
     return transformElementDOM(path, info);
