a0eb988b7c70291adbf1224632bf79771712a10d
Tito <tito.bouzout@gmail.com>
2023-03-08 08:05:26 -0300

    remove caching

--- packages/dom-expressions/src/client.js
+++ packages/dom-expressions/src/client.js
@@ -58,30 +58,22 @@ export function render(code, element, init, options = {}) {
   };
 }
 
-export const template = function template(
-  templates,
-  html,
-  check,
-  isSVG,
-) {
+export function template(html, check, isSVG) {
   return {
-    cloneNode: e => {
-      if (!templates.has(html)) {
-        const t = document.createElement('template')
-        t.innerHTML = html
-        if ('_DX_DEV_' && check && t.innerHTML.split('<').length - 1 !== check)
-          throw `The browser resolved template HTML does not match JSX input:\n${t.innerHTML}\n\n${html}. Is your HTML properly formed?`
-        templates.set(
-          html,
-          isSVG
-            ? t.content.firstChild.firstChild
-            : t.content.firstChild,
-        )
-      }
-      return templates.get(html).cloneNode(e)
+    cloneNode: function (e) {
+      const t = document.createElement('template')
+      t.innerHTML = html
+      if ('_DX_DEV_' && check && t.innerHTML.split('<').length - 1 !== check)
+        throw `The browser resolved template HTML does not match JSX input:\n${t.innerHTML}\n\n${html}. Is your HTML properly formed?`
+      const node = isSVG
+        ? t.content.firstChild.firstChild
+        : t.content.firstChild
+
+      this.cloneNode = node.cloneNode.bind(node)
+      return this.cloneNode(e)
     },
   }
-}.bind(null, new Map())
+}
 
 export function delegateEvents(eventNames, document = window.document) {
   const e = document[$$EVENTS] || (document[$$EVENTS] = new Set());
