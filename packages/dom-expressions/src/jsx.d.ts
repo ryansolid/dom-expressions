8393201f163b84ea8eacf644ac6f14b543f1963c
Ishank Sharma <ishank.web.dev@gmail.com>
2023-04-09 19:23:56 +0530

    Update jsx.d.ts
    added allow-top-navigation-to-custom-protocols to iframe

--- packages/dom-expressions/src/jsx.d.ts
+++ packages/dom-expressions/src/jsx.d.ts
@@ -405,7 +405,8 @@ export namespace JSX {
     | "allow-scripts"
     | "allow-storage-access-by-user-activation"
     | "allow-top-navigation"
-    | "allow-top-navigation-by-user-activation";
+    | "allow-top-navigation-by-user-activation"
+    | "allow-top-navigation-to-custom-protocols;
   type HTMLLinkAs =
     | "audio"
     | "document"
