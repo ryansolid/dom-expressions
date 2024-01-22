693da2b6ce58a38bcd382b26d32d02225781b658
Tito <tito.bouzout@gmail.com>
2024-01-22 19:40:55 -0300

    Friendly message for when mount point doesn't exists (#306)
    * Update client.js
    
    * Update client.js
    
    * Update client.js

--- packages/dom-expressions/src/client.js
+++ packages/dom-expressions/src/client.js
@@ -46,6 +46,11 @@ export {
 };
 
 export function render(code, element, init, options = {}) {
+  if(!element && "_DX_DEV_") {
+     throw new Error(
+        "The `element` passed to `render(..., element)` doesn't exist in the document. Make sure `element` exists in the document."
+     );
+  }
   let disposer;
   root(dispose => {
     disposer = dispose;
