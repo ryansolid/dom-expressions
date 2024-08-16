0bf8808e996cd1ec50b119aac59c02aa32aa44fb
Ryan Carniato <ryansolid@gmail.com>
2024-08-16 09:37:07 -0700

    update universal renderer

--- packages/dom-expressions/src/client.js
+++ packages/dom-expressions/src/client.js
@@ -205,8 +205,8 @@ export function style(node, value, prev) {
   return prev;
 }
 
+// TODO: make this better
 export function spread(node, props = {}, isSVG, skipChildren) {
-  // TODO: make this better
   const prevProps = {};
   if (!skipChildren) {
     effect(
@@ -222,8 +222,13 @@ export function spread(node, props = {}, isSVG, skipChildren) {
     () => ({})
   );
   effect(() => {
-    for (const prop in props) { props[prop]; }
-  }, () => assign(node, props, isSVG, true, prevProps, true));
+    const newProps = {};
+    for (const prop in props) {
+      if (prop === "children" || prop === "ref") continue;
+      newProps[prop] = props[prop];
+    }
+    return newProps;
+  }, props => assign(node, props, isSVG, true, prevProps, true));
   return prevProps;
 }
 
