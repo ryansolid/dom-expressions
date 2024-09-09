5790bfcd0d80678fa82e5dee98cbc29af185b99d
Ryan Carniato <ryansolid@gmail.com>
2024-09-09 15:04:19 -0700

    fix #344: missing semicolon

--- packages/dom-expressions/src/server.js
+++ packages/dom-expressions/src/server.js
@@ -29,7 +29,7 @@ export function renderToString(code, options = {}) {
       if (!scripts) {
         scripts = getLocalHeaderScript(renderId);
       }
-      scripts += script;
+      scripts += script + ";";
     },
     onError: options.onError
   });
