5470fbe0c471cdeb93322492c5e3e0e210241e7b
Tito Bouzout <tito.bouzout@gmail.com>
2024-09-25 03:11:01 -0300

    format jsdoc comments

--- packages/dom-expressions/src/jsx.d.ts
+++ packages/dom-expressions/src/jsx.d.ts
@@ -157,9 +157,7 @@ export namespace JSX {
   interface ExplicitAttributes {}
   interface ExplicitBoolAttributes {}
   interface CustomEvents {}
-  /**
-   * @deprecated Replaced by CustomEvents
-  */
+  /** @deprecated Replaced by CustomEvents */
   interface CustomCaptureEvents {}
   type DirectiveAttributes = {
     [Key in keyof Directives as `use:${Key}`]?: Directives[Key];
@@ -316,9 +314,7 @@ export namespace JSX {
     onWaiting?: EventHandlerUnion<T, Event> | undefined;
     onWheel?: EventHandlerUnion<T, WheelEvent> | undefined;
   }
-  /**
-   * @type {GlobalEventHandlers}
-   */
+  /** @type {GlobalEventHandlers} */
   interface CustomEventHandlersLowerCase<T> {
     onabort?: EventHandlerUnion<T, Event> | undefined;
     onanimationend?: EventHandlerUnion<T, AnimationEvent> | undefined;
@@ -452,43 +448,64 @@ export namespace JSX {
 
   // All the WAI-ARIA 1.1 attributes from https://www.w3.org/TR/wai-aria-1.1/
   interface AriaAttributes {
-    /** Identifies the currently active element when DOM focus is on a composite widget, textbox, group, or application. */
+    /**
+     * Identifies the currently active element when DOM focus is on a composite widget, textbox,
+     * group, or application.
+     */
     "aria-activedescendant"?: string | undefined;
-    /** Indicates whether assistive technologies will present all, or only parts of, the changed region based on the change notifications defined by the aria-relevant attribute. */
+    /**
+     * Indicates whether assistive technologies will present all, or only parts of, the changed
+     * region based on the change notifications defined by the aria-relevant attribute.
+     */
     "aria-atomic"?: boolean | "false" | "true" | undefined;
     /**
-     * Indicates whether inputting text could trigger display of one or more predictions of the user's intended value for an input and specifies how predictions would be
-     * presented if they are made.
+     * Indicates whether inputting text could trigger display of one or more predictions of the
+     * user's intended value for an input and specifies how predictions would be presented if they
+     * are made.
      */
     "aria-autocomplete"?: "none" | "inline" | "list" | "both" | undefined;
-    /** Indicates an element is being modified and that assistive technologies MAY want to wait until the modifications are complete before exposing them to the user. */
+    /**
+     * Indicates an element is being modified and that assistive technologies MAY want to wait until
+     * the modifications are complete before exposing them to the user.
+     */
     "aria-busy"?: boolean | "false" | "true" | undefined;
     /**
      * Indicates the current "checked" state of checkboxes, radio buttons, and other widgets.
+     *
      * @see aria-pressed @see aria-selected.
      */
     "aria-checked"?: boolean | "false" | "mixed" | "true" | undefined;
     /**
      * Defines the total number of columns in a table, grid, or treegrid.
+     *
      * @see aria-colindex.
      */
     "aria-colcount"?: number | string | undefined;
     /**
-     * Defines an element's column index or position with respect to the total number of columns within a table, grid, or treegrid.
+     * Defines an element's column index or position with respect to the total number of columns
+     * within a table, grid, or treegrid.
+     *
      * @see aria-colcount @see aria-colspan.
      */
     "aria-colindex"?: number | string | undefined;
     /**
-     * Defines the number of columns spanned by a cell or gridcell within a table, grid, or treegrid.
+     * Defines the number of columns spanned by a cell or gridcell within a table, grid, or
+     * treegrid.
+     *
      * @see aria-colindex @see aria-rowspan.
      */
     "aria-colspan"?: number | string | undefined;
     /**
-     * Identifies the element (or elements) whose contents or presence are controlled by the current element.
+     * Identifies the element (or elements) whose contents or presence are controlled by the current
+     * element.
+     *
      * @see aria-owns.
      */
     "aria-controls"?: string | undefined;
-    /** Indicates the element that represents the current item within a container or set of related elements. */
+    /**
+     * Indicates the element that represents the current item within a container or set of related
+     * elements.
+     */
     "aria-current"?:
       | boolean
       | "false"
@@ -501,42 +518,57 @@ export namespace JSX {
       | undefined;
     /**
      * Identifies the element (or elements) that describes the object.
+     *
      * @see aria-labelledby
      */
     "aria-describedby"?: string | undefined;
     /**
      * Identifies the element that provides a detailed, extended description for the object.
+     *
      * @see aria-describedby.
      */
     "aria-details"?: string | undefined;
     /**
-     * Indicates that the element is perceivable but disabled, so it is not editable or otherwise operable.
+     * Indicates that the element is perceivable but disabled, so it is not editable or otherwise
+     * operable.
+     *
      * @see aria-hidden @see aria-readonly.
      */
     "aria-disabled"?: boolean | "false" | "true" | undefined;
     /**
-     * Indicates what functions can be performed when a dragged object is released on the drop target.
-     * @deprecated in ARIA 1.1
+     * Indicates what functions can be performed when a dragged object is released on the drop
+     * target.
+     *
+     * @deprecated In ARIA 1.1
      */
     "aria-dropeffect"?: "none" | "copy" | "execute" | "link" | "move" | "popup" | undefined;
     /**
      * Identifies the element that provides an error message for the object.
+     *
      * @see aria-invalid @see aria-describedby.
      */
     "aria-errormessage"?: string | undefined;
-    /** Indicates whether the element, or another grouping element it controls, is currently expanded or collapsed. */
+    /**
+     * Indicates whether the element, or another grouping element it controls, is currently expanded
+     * or collapsed.
+     */
     "aria-expanded"?: boolean | "false" | "true" | undefined;
     /**
-     * Identifies the next element (or elements) in an alternate reading order of content which, at the user's discretion,
-     * allows assistive technology to override the general default of reading in document source order.
+     * Identifies the next element (or elements) in an alternate reading order of content which, at
+     * the user's discretion, allows assistive technology to override the general default of reading
+     * in document source order.
      */
     "aria-flowto"?: string | undefined;
     /**
      * Indicates an element's "grabbed" state in a drag-and-drop operation.
-     * @deprecated in ARIA 1.1
+     *
+     * @deprecated In ARIA 1.1
      */
     "aria-grabbed"?: boolean | "false" | "true" | undefined;
-    /** Indicates the availability and type of interactive popup element, such as menu or dialog, that can be triggered by an element. */
+    /**
+     * Indicates the availability and type of interactive popup element, such as menu or dialog,
+     * that can be triggered by an element.
+     */
     "aria-haspopup"?:
       | boolean
       | "false"
@@ -549,66 +581,88 @@ export namespace JSX {
       | undefined;
     /**
      * Indicates whether the element is exposed to an accessibility API.
+     *
      * @see aria-disabled.
      */
     "aria-hidden"?: boolean | "false" | "true" | undefined;
     /**
      * Indicates the entered value does not conform to the format expected by the application.
+     *
      * @see aria-errormessage.
      */
     "aria-invalid"?: boolean | "false" | "true" | "grammar" | "spelling" | undefined;
-    /** Indicates keyboard shortcuts that an author has implemented to activate or give focus to an element. */
+    /**
+     * Indicates keyboard shortcuts that an author has implemented to activate or give focus to an
+     * element.
+     */
     "aria-keyshortcuts"?: string | undefined;
     /**
      * Defines a string value that labels the current element.
+     *
      * @see aria-labelledby.
      */
     "aria-label"?: string | undefined;
     /**
      * Identifies the element (or elements) that labels the current element.
+     *
      * @see aria-describedby.
      */
     "aria-labelledby"?: string | undefined;
     /** Defines the hierarchical level of an element within a structure. */
     "aria-level"?: number | string | undefined;
-    /** Indicates that an element will be updated, and describes the types of updates the user agents, assistive technologies, and user can expect from the live region. */
+    /**
+     * Indicates that an element will be updated, and describes the types of updates the user
+     * agents, assistive technologies, and user can expect from the live region.
+     */
     "aria-live"?: "off" | "assertive" | "polite" | undefined;
     /** Indicates whether an element is modal when displayed. */
     "aria-modal"?: boolean | "false" | "true" | undefined;
     /** Indicates whether a text box accepts multiple lines of input or only a single line. */
     "aria-multiline"?: boolean | "false" | "true" | undefined;
-    /** Indicates that the user may select more than one item from the current selectable descendants. */
+    /**
+     * Indicates that the user may select more than one item from the current selectable
+     * descendants.
+     */
     "aria-multiselectable"?: boolean | "false" | "true" | undefined;
     /** Indicates whether the element's orientation is horizontal, vertical, or unknown/ambiguous. */
     "aria-orientation"?: "horizontal" | "vertical" | undefined;
     /**
-     * Identifies an element (or elements) in order to define a visual, functional, or contextual parent/child relationship
-     * between DOM elements where the DOM hierarchy cannot be used to represent the relationship.
+     * Identifies an element (or elements) in order to define a visual, functional, or contextual
+     * parent/child relationship between DOM elements where the DOM hierarchy cannot be used to
+     * represent the relationship.
+     *
      * @see aria-controls.
      */
     "aria-owns"?: string | undefined;
     /**
-     * Defines a short hint (a word or short phrase) intended to aid the user with data entry when the control has no value.
-     * A hint could be a sample value or a brief description of the expected format.
+     * Defines a short hint (a word or short phrase) intended to aid the user with data entry when
+     * the control has no value. A hint could be a sample value or a brief description of the
+     * expected format.
      */
     "aria-placeholder"?: string | undefined;
     /**
-     * Defines an element's number or position in the current set of listitems or treeitems. Not required if all elements in the set are present in the DOM.
+     * Defines an element's number or position in the current set of listitems or treeitems. Not
+     * required if all elements in the set are present in the DOM.
+     *
      * @see aria-setsize.
      */
     "aria-posinset"?: number | string | undefined;
     /**
      * Indicates the current "pressed" state of toggle buttons.
+     *
      * @see aria-checked @see aria-selected.
      */
     "aria-pressed"?: boolean | "false" | "mixed" | "true" | undefined;
     /**
      * Indicates that the element is not editable, but is otherwise operable.
+     *
      * @see aria-disabled.
      */
     "aria-readonly"?: boolean | "false" | "true" | undefined;
     /**
-     * Indicates what notifications the user agent will trigger when the accessibility tree within a live region is modified.
+     * Indicates what notifications the user agent will trigger when the accessibility tree within a
+     * live region is modified.
+     *
      * @see aria-atomic.
      */
     "aria-relevant"?:
@@ -629,26 +683,33 @@ export namespace JSX {
     "aria-roledescription"?: string | undefined;
     /**
      * Defines the total number of rows in a table, grid, or treegrid.
+     *
      * @see aria-rowindex.
      */
     "aria-rowcount"?: number | string | undefined;
     /**
-     * Defines an element's row index or position with respect to the total number of rows within a table, grid, or treegrid.
+     * Defines an element's row index or position with respect to the total number of rows within a
+     * table, grid, or treegrid.
+     *
      * @see aria-rowcount @see aria-rowspan.
      */
     "aria-rowindex"?: number | string | undefined;
     /**
      * Defines the number of rows spanned by a cell or gridcell within a table, grid, or treegrid.
+     *
      * @see aria-rowindex @see aria-colspan.
      */
     "aria-rowspan"?: number | string | undefined;
     /**
      * Indicates the current "selected" state of various widgets.
+     *
      * @see aria-checked @see aria-pressed.
      */
     "aria-selected"?: boolean | "false" | "true" | undefined;
     /**
-     * Defines the number of items in the current set of listitems or treeitems. Not required if all elements in the set are present in the DOM.
+     * Defines the number of items in the current set of listitems or treeitems. Not required if all
+     * elements in the set are present in the DOM.
+     *
      * @see aria-posinset.
      */
     "aria-setsize"?: number | string | undefined;
@@ -660,6 +721,7 @@ export namespace JSX {
     "aria-valuemin"?: number | string | undefined;
     /**
      * Defines the current value for a range widget.
+     *
      * @see aria-valuetext.
      */
     "aria-valuenow"?: number | string | undefined;
@@ -2040,9 +2102,7 @@ export namespace JSX {
     textLength?: number | string | undefined;
     lengthAdjust?: "spacing" | "spacingAndGlyphs" | undefined;
   }
-  /**
-   * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Element/use
-   */
+  /** @see https://developer.mozilla.org/en-US/docs/Web/SVG/Element/use */
   interface UseSVGAttributes<T>
     extends CoreSVGAttributes<T>,
       StylableSVGAttributes,
@@ -2064,9 +2124,7 @@ export namespace JSX {
       ZoomAndPanSVGAttributes {
     viewTarget?: string | undefined;
   }
-  /**
-   * @type {HTMLElementTagNameMap}
-   */
+  /** @type {HTMLElementTagNameMap} */
   interface HTMLElementTags {
     a: AnchorHTMLAttributes<HTMLAnchorElement>;
     abbr: HTMLAttributes<HTMLElement>;
@@ -2181,9 +2239,7 @@ export namespace JSX {
     video: VideoHTMLAttributes<HTMLVideoElement>;
     wbr: HTMLAttributes<HTMLElement>;
   }
-  /**
-   * @type {HTMLElementDeprecatedTagNameMap}
-   */
+  /** @type {HTMLElementDeprecatedTagNameMap} */
   interface HTMLElementDeprecatedTags {
     big: HTMLAttributes<HTMLElement>;
     keygen: KeygenHTMLAttributes<HTMLElement>;
@@ -2191,9 +2247,7 @@ export namespace JSX {
     noindex: HTMLAttributes<HTMLElement>;
     param: ParamHTMLAttributes<HTMLParamElement>;
   }
-  /**
-   * @type {SVGElementTagNameMap}
-   */
+  /** @type {SVGElementTagNameMap} */
   interface SVGElementTags {
     animate: AnimateSVGAttributes<SVGAnimateElement>;
     animateMotion: AnimateMotionSVGAttributes<SVGAnimateMotionElement>;
