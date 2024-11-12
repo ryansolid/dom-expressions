de55d02aba88673fedc8126c28b584d3d685a56c
Tito Bouzout <tito.bouzout@gmail.com>
2024-11-12 10:14:01 -0300

    sort list

--- packages/dom-expressions/src/jsx.d.ts
+++ packages/dom-expressions/src/jsx.d.ts
@@ -1459,11 +1459,11 @@ export namespace JSX {
     controls?: "true" | boolean | undefined;
     controlslist?: string | undefined;
     crossorigin?: HTMLCrossorigin | undefined;
+    disableremoteplayback?: "true" | boolean | undefined;
     loop?: "true" | boolean | undefined;
     muted?: "true" | boolean | undefined;
     preload?: "none" | "metadata" | "auto" | "" | undefined;
     src?: string | undefined;
-    disableremoteplayback?: "true" | boolean | undefined;
 
     /** @deprecated Use lowercase attributes */
     crossOrigin?: HTMLCrossorigin | undefined;
