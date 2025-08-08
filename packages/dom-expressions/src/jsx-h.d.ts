448726c059d1deabee63a2c157a0a2a8535b45ac
Tito Bouzout <tito.bouzout@gmail.com>
2025-08-08 06:16:22 -0300

    remove unused types related to `bool:` and `attr:`

--- packages/dom-expressions/src/jsx-h.d.ts
+++ packages/dom-expressions/src/jsx-h.d.ts
@@ -249,8 +249,6 @@ export namespace JSX {
     [x: string]: (el: DOMElement, accessor: Accessor<any>) => void;
   }
   interface ExplicitProperties {}
-  interface ExplicitAttributes {}
-  interface ExplicitBoolAttributes {}
   interface CustomEvents {}
   type DirectiveAttributes = {
     [Key in keyof Directives as `use:${Key}`]?: Directives[Key];
@@ -1037,8 +1035,6 @@ export namespace JSX {
       DirectiveAttributes,
       DirectiveFunctionAttributes<T>,
       PropAttributes,
-      AttrAttributes,
-      BoolAttributes,
       OnAttributes<T>,
       EventHandlersElement<T>,
       AriaAttributes {
