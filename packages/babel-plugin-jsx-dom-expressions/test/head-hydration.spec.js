4c9f01f7a7ae14567797792dd857f8b8252301c6
Birk Skyum <birk.skyum@pm.me>
2026-01-06 00:43:33 +0100

    test

---
+++ packages/babel-plugin-jsx-dom-expressions/test/head-hydration.spec.js
@@ -0,0 +1,53 @@
+const babel = require("@babel/core");
+const plugin = require("../index");
+
+const code = `
+const template = (
+  <html>
+    <head>
+      <Assets />
+    </head>
+    <body>
+      <App />
+    </body>
+  </html>
+);
+`;
+
+const baseOptions = {
+  builtIns: ["For", "Show"],
+  hydratable: true,
+  contextToCustomElements: true,
+  staticMarker: "@once"
+};
+
+function transform(options) {
+  return babel.transformSync(code, {
+    babelrc: false,
+    configFile: false,
+    parserOpts: { plugins: ["jsx"] },
+    plugins: [[plugin, options]]
+  }).code;
+}
+
+describe("head hydration", () => {
+  it("keeps head hydratable in ssr output", () => {
+    const output = transform({
+      ...baseOptions,
+      moduleName: "r-server",
+      generate: "ssr"
+    });
+
+    expect(output).not.toMatch(/NoHydration/);
+  });
+
+  it("keeps head hydratable in dom output", () => {
+    const output = transform({
+      ...baseOptions,
+      moduleName: "r-dom",
+      generate: "dom"
+    });
+
+    expect(output).not.toMatch(/NoHydration/);
+  });
+});
